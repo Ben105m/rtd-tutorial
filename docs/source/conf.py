b8370b145c2d02500499b825190a163b0e307a1e
Ben105m <119285556+Ben105m@users.noreply.github.com>
2023-07-27 16:03:48 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PyDrag'
+copyright = '2023, IRSN'
+author = 'IRSN'
 
 release = '0.1'
 version = '0.1.0'
